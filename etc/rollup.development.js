38a162d220534bb2a8660e77a78313daaedf0105
petecarapetyan <pete@datafundamentals.com>
2020-04-06 06:35:41 -0500

    feat(rollup): misc rollup enhancements

--- etc/rollup.development.js
+++ etc/rollup.development.js
@@ -1,14 +1,44 @@
 import resolve from "@rollup/plugin-node-resolve";
+import minify from 'rollup-plugin-minify-html-literals';
 import typescript from "@rollup/plugin-typescript";
+import analyze from "rollup-plugin-analyzer";
+import copy from "rollup-plugin-copy";
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from 'rollup-plugin-terser';
+import size from "rollup-plugin-size";
 
 module.exports = {
     input: "./src/index.ts",
     output: {
         dir: "./public/scripts",
-        format: "esm"
+        format: "esm",
     },
     plugins: [
-        resolve({ dedupe: ["lit-element", "lit-html"] }),
+        commonjs(),
+        copy({
+            targets: [{ src: "/asset", dest: "public" }],
+        }),
+        resolve({dedupe: [
+            '@captaincodeman/rdx',
+            '@captaincodeman/rdx-model',
+            '@captaincodeman/router',
+            'lit-element',
+            'lit-html',
+          ] }),
         typescript(),
-    ]
+        minify({
+          options: {
+            shouldMinify: (template) => defaultShouldMinify(template) || 
+                                        template.parts.some(part => part.text.includes('<style')) ||
+                                        template.parts.some(part => part.text.includes('</span'))
+          }
+        }),
+        terser({
+            output: {
+                comments: false
+            }
+        }),
+        analyze(),
+        size()
+    ],
 };
